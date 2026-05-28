38adb3952147fa3476e3df06b69807ab5f9455f7
Lain <chiyomiziwari@gmail.com>
2026-05-28 10:05:43 +0700

    constants

---
+++ BlazorApp/Helpers/Constants/OrderStatuses.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorApp.Helpers.Constants
+{
+    public static class OrderStatuses
+    {
+        public const string Unpaid = "Unpaid";
+        public const string Paid = "Paid";
+        public const string Cancelled = "Cancelled";
+
+        public static string ViTranslate(string status)
+        {
+            var viDict = new Dictionary<string, string>()
+            {
+                {Paid, "Đã thanh toán" },
+                {Unpaid, "Chưa thanh toán" },
+                {Cancelled, "Đã hủy" }
+            };
+            return viDict[status];
+        }
+    }
+}
