1c1ea3d8c1d08fdc2b89b5f858b1699d743cfdff
Lain <chiyomiziwari@gmail.com>
2026-05-31 12:41:20 +0700

    sửa order/

--- WebAPI/Services/Implementations/UserService.cs
+++ WebAPI/Services/Implementations/UserService.cs
@@ -20,26 +20,6 @@ namespace WebAPI.Services.Implementations
             _mapper = mapper;
         }
 
-        public async Task<CustomerResult<CustomerDTO>> GetCustomerByIdAsync(Guid customerId)
-        {
-            var customer = await _context.Customers
-                .FirstOrDefaultAsync(c => c.PublicId == customerId);
-            if(customer == null)
-            {
-                return new CustomerResult<CustomerDTO>
-                {
-                    Success = false,
-                    Message = "Khách hàng không tồn tại."
-                };
-            }
-            return new CustomerResult<CustomerDTO>
-            {
-                Success = true,
-                Message = "Lấy thông tin khách hàng thành công.",
-                Data = _mapper.Map<CustomerDTO>(customer)
-            };
-        }
-
         public async Task<CustomerResult<List<CustomerDTO>>> GetCustomerListAsync()
         {
             var customers = await _context.Customers
@@ -52,27 +32,6 @@ namespace WebAPI.Services.Implementations
             };
         }
 
-        public async Task<ManagerResult<ManagerDTO>> GetEmployeeByIdAsync(Guid employeeId)
-        {
-            var employee = await _context.Employees
-                .Include(e => e.User)
-                .FirstOrDefaultAsync(e => e.PublicId == employeeId);
-            if(employee == null)
-            {
-                return new ManagerResult<ManagerDTO>
-                {
-                    Success = false,
-                    Message = "Nhân viên không tồn tại."
-                };
-            }
-            return new ManagerResult<ManagerDTO>
-            {
-                Success = true,
-                Message = "Lấy thông tin nhân viên thành công.",
-                Data = _mapper.Map<ManagerDTO>(employee)
-            };
-        }
-
         public async Task<ManagerResult<List<ManagerDTO>>> GetEmployeeListAsync()
         {
             var employees = await _context.Employees
@@ -82,50 +41,6 @@ namespace WebAPI.Services.Implementations
                 Success = true,
                 Message = "Lấy danh sách nhân viên thành công.",
                 Data = _mapper.Map<List<ManagerDTO>>(employees)
-            };
-        }
-
-        public async Task<AccountResult> PasswordChangeAsync(Guid id, PasswordChangeDTO dto)
-        {
-            var manager = await _context.Employees
-                .Include(c => c.User)
-                .FirstOrDefaultAsync(c => c.User.PublicId == id);
-
-            if (manager == null || manager.User.IsActive == false)
-            {
-                return new AccountResult
-                {
-                    Success = false,
-                    Message = "Tài khoản không tồn tại hoặc đã bị khóa!"
-                };
-            }
-
-            if (!BCrypt.Net.BCrypt.Verify(dto.CurrentPassword, manager.User.PasswordHash))
-            {
-                return new AccountResult
-                {
-                    Success = false,
-                    Message = "Mật khẩu hiện tại không chính xác."
-                };
-            }
-
-            if (dto.NewPassword != dto.ConfirmNewPassword)
-            {
-                return new AccountResult
-                {
-                    Success = false,
-                    Message = "Mật khẩu mới và xác nhận mật khẩu mới không khớp."
-                };
-            }
-
-            manager.User.PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.NewPassword);
-            _context.Employees.Update(manager);
-            await _context.SaveChangesAsync();
-
-            return new AccountResult
-            {
-                Success = true,
-                Message = "Đổi mật khẩu thành công."
             };
         }
     }
--- WebAPI/Services/Interfaces/IUserService.cs
+++ WebAPI/Services/Interfaces/IUserService.cs
@@ -8,12 +8,8 @@ namespace WebAPI.Services.Interfaces
     public interface IUserService
     {
         Task<ManagerResult<List<ManagerDTO>>> GetEmployeeListAsync();
-        Task<ManagerResult<ManagerDTO>> GetEmployeeByIdAsync(Guid employeeId);
 
         Task<CustomerResult<List<CustomerDTO>>> GetCustomerListAsync();
-        Task<CustomerResult<CustomerDTO>> GetCustomerByIdAsync(Guid customerId);
-
-        Task<AccountResult> PasswordChangeAsync(Guid userId, PasswordChangeDTO dto);
         
     }
 }
